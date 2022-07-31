351668b611d00ee9341870f728340b54fa8c3f5f
MiratRakhmanov <miratrakhmanov@gmail.com>
2022-07-30 21:25:13 -0500

    Initial commit

---
+++ tests/designory/test.js
@@ -0,0 +1,24 @@
+module.exports = {
+    before: function (browser) {
+        console.log('Setting up...');
+        browser.page.home().navigate();
+    },
+
+    "Menu Verification": async function (browser) {
+        const menuOptions = browser.globals.menuOptions
+        const pageTitles = browser.globals.pageTitles
+        const homePage = browser.page.home();
+
+        let actualOptions = await homePage.getAllMenuOptions()
+        browser.assert.ok(actualOptions.length == 6, "array has length of 6")
+
+        let result = await homePage.getMenuOptionsAndPageTitlesFromAllPages(menuOptions)
+        browser.assert.ok(await homePage.compareMenuOptions(result[0], menuOptions), "menu options are equal")
+        browser.assert.ok(await homePage.checkArraysEqual(result[1], pageTitles), "page titles are equal")
+    },
+
+    after: function (browser) {
+        console.log('Closing down...');
+        browser.quit();
+    }
+}
---
+++ tests/page-objects/home.js
@@ -0,0 +1,110 @@
+const homePageCommands = {
+
+    openMenuToggle: async function () {
+        const result = await this.isVisible('@menuWindow')
+        if (result == false) {
+            await this.click('@menuToggle').waitForElementVisible('@menuWindow', 1000)
+        }
+        return this
+    },
+
+    getAllMenuOptions: async function () {
+        await this.openMenuToggle()
+        const jsonElements = await this.findElements('@menuItems')
+        const ids = jsonElements.map(function (jsonElement) { return jsonElement.getId() })
+        const actualOptions = []
+        for (let id of ids) {
+            const elementValue = await browser.elementIdText(id, function (element) { return element.value })
+            actualOptions.push(elementValue)
+        }
+        return actualOptions
+    },
+
+    getMenuOptionsAndPageTitlesFromAllPages: async function (menuOptions) {
+        let options = []
+        let titles = []
+        for (let option of menuOptions) {
+            await this.openMenuToggle()
+            this.navigateTo(option)
+            let actualOptions = await this.getAllMenuOptions(menuOptions)
+            options.push(actualOptions)
+            let title = await this.getTitle(function (title) { return title });
+            titles.push(title)
+        }
+        return [options, titles]
+    },
+
+    navigateTo: function (menuOption) {
+        const locator = "//a[text()='" + menuOption + "']"
+        this
+            .waitForElementVisible("xpath", locator, 2000)
+            .click("xpath", locator)
+        if (menuOption === "LOCATIONS") {
+            const defaultCity = browser.globals.defaultCity
+            const cityLocator = "//a[text()='" + defaultCity + "']"
+            this
+                .waitForElementVisible('@locationsMenu', 2000)
+                .click("xpath", cityLocator)
+        }
+        return this
+    },
+
+    compareMenuOptions: async function (actual, expected) {
+        let returnBoolean = true
+        for (let option of actual) {
+            let result = await this.checkArraysEqual(option, expected)
+            if (result == false) {
+                returnBoolean = false
+                return false
+            }
+        }
+        return returnBoolean
+    },
+
+    checkArraysEqual: async function (a, b) {
+        return JSON.stringify(a) === JSON.stringify(b)
+    },
+
+    getMenuOptionsFromAllPages: async function (menuOptions) {
+        let returnBoolean = true
+        for (let option of menuOptions) {
+            await this.openMenuToggle()
+            this.navigateTo(option)
+            let actualOptions = await this.getAllMenuOptions(menuOptions)
+            let result = await this.checkArraysEqual(menuOptions, actualOptions)
+            if (result == false) {
+                returnBoolean = false
+                return false
+            }
+        }
+        return returnBoolean
+    },
+
+    getPageTitles: async function (menuOptions) {
+        let titles = []
+        for (let menuOption of menuOptions) {
+            let title = await this.getPageTitle(menuOption)
+            titles.push(title)
+        }
+        return titles
+    },
+
+    getPageTitle: async function (menuOption) {
+        await this.openMenuToggle()
+        this.navigateTo(menuOption)
+        return await this.getTitle(function (title) { return title });
+    }
+}
+
+module.exports = {
+    url: 'https://www.designory.com',
+
+    commands: [homePageCommands],
+
+    elements: {
+        menuToggle: '#nav-toggle',
+        menuItems: 'div.nav-wrapper.scroll>nav>ul>li>a',
+        menuWindow: '.nav-wrapper.scroll',
+        locationsMenu: ".subnav"
+    }
+}
